c922abb804552b23ce5c1936dec95227757bc289
williamd8323 <williamd8323@afsenyc.org>
2017-07-21 13:36:24 -0400

    chages to game js

--- game.js
+++ game.js
@@ -6,32 +6,57 @@ function Ship(){
 
     this.show = function(){
         fill(255,255,255);
-        rect(this.x,height-20,40,20);
+        rect(this.x,height-30,40,20);
     }
 
     this.move = function(m){
-        this.x+=m;
+       if(this.x <= 0){
+            this.x+=10;
+       }
+        else if(this.x >= width - 40){
+            this.x-=10;
+        }
+        else{
+            this.x+=m;
+        }
+    }
+}
+
+function Alien(x,y){
+    this.x = x;
+    this.y = y;
+
+    this.show = function(){
+        fill(0,255,0);
+        
     }
 }
 
 function setup(){
-    createCanvas(1000,400);
+    background('black');
+    createCanvas(600,700);
     ship = new Ship();
 }
 
 function draw(){
-    background(0);
+    background('gray');
     ship.show();
+    if(keyIsDown(RIGHT_ARROW)){
+        ship.move(10);
+    } 
+    else if(keyIsDown(LEFT_ARROW)){
+        ship.move(-10);
+    }
 }
 
+/*
 function keyPressed(){
     if(keyCode === RIGHT_ARROW){
-        ship.move(5);
+        ship.move(10);
     } 
     else if(keyCode === LEFT_ARROW){
-        ship.move(-5);
-    }
-    else{
-
+        ship.move(-10);
     }
 }
+
+*/
